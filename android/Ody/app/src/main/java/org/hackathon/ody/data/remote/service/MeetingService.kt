7839a38417b37813a4e5ad0f8f25e906eeb4d87f
haeum808 <haeumueah@gmail.com>
2024-07-15 21:39:23 +0900

    chore: MeetingRoomResponse 반환값으로 변경

--- android/Ody/app/src/main/java/org/hackathon/ody/data/remote/service/MeetingService.kt
+++ android/Ody/app/src/main/java/org/hackathon/ody/data/remote/service/MeetingService.kt
@@ -1,7 +1,7 @@
 package org.hackathon.ody.data.remote.service
 
 import org.hackathon.ody.data.remote.reqeust.MeetingInfoRequest
-import org.hackathon.ody.data.remote.response.MeetingInfoResponse
+import org.hackathon.ody.data.remote.response.MeetingRoomResponse
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
@@ -11,7 +11,7 @@ interface MeetingService {
     @POST("${MEETING_RELATIVE_URL}/")
     suspend fun postMeetingRoom(
         @Body body: MeetingInfoRequest
-    ): Response<MeetingInfoResponse>
+    ): Response<MeetingRoomResponse>
 
     companion object {
         private const val MEETING_RELATIVE_URL = ""
