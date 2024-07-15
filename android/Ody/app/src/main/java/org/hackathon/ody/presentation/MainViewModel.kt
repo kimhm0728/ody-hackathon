1916be12c7582ecdc8f612878d11519f59a5603d
haeum808 <haeumueah@gmail.com>
2024-07-15 22:36:59 +0900

    feat: MainVieWModel 구현

--- android/Ody/app/src/main/java/org/hackathon/ody/presentation/MainViewModel.kt
+++ android/Ody/app/src/main/java/org/hackathon/ody/presentation/MainViewModel.kt
@@ -1,7 +1,11 @@
 package org.hackathon.ody.presentation
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel: ViewModel() {
-
+    val meetingName = MutableLiveData<String>()
+    val meetingDate = MutableLiveData<String>()
+    val meetingTime = MutableLiveData<String>()
+    val nickname = MutableLiveData<String>()
 }
