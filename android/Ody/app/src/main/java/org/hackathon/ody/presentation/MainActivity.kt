7fbe9a7ea82335d1a014962d1a1fda98a768d57f
aprilgom <aprilgom@gmail.com>
2024-07-15 20:42:11 +0900

    feat: 알림 권한 요청 기능 구현

--- android/Ody/app/src/main/java/org/hackathon/ody/presentation/MainActivity.kt
+++ android/Ody/app/src/main/java/org/hackathon/ody/presentation/MainActivity.kt
@@ -1,7 +1,12 @@
 package org.hackathon.ody.presentation
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.viewpager2.widget.ViewPager2
 import org.hackathon.ody.R
 
@@ -15,5 +20,29 @@ class MainActivity : AppCompatActivity() {
         val pagerAdapter = MeetingInfoAdapter(this)
         viewPager = findViewById(R.id.pager)
         viewPager.adapter = pagerAdapter
+        requestNotificationPermission()
     }
+
+    private fun requestNotificationPermission() {
+        if (ContextCompat.checkSelfPermission(
+                this, Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
+                    // 권한 요청 거부한 경우
+                } else {
+                    requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+                }
+            } else {
+                // 안드로이드 12 이하는 Notification에 관한 권한 필요 없음
+            }
+        }
+    }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+    }
+
 }
