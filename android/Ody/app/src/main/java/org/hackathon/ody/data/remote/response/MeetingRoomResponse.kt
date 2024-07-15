05d1cf5945083951c0420302ae896ba6ea3d8e97
haeum808 <haeumueah@gmail.com>
2024-07-15 21:03:58 +0900

    feat: MeetingRoomResponse Json 형식 변경

---
+++ android/Ody/app/src/main/java/org/hackathon/ody/data/remote/response/MeetingRoomResponse.kt
@@ -0,0 +1,26 @@
+package org.hackathon.ody.data.remote.response
+
+import com.squareup.moshi.Json
+import java.time.LocalDate
+import java.time.LocalTime
+
+data class MeetingRoomResponse(
+    @Json(name = "id") val id: Long,
+    @Json(name = "name") val name: String,
+    @Json(name = "date") val date: LocalDate,
+    @Json(name = "time") val time: LocalTime,
+    @Json(name = "targetAddress") val address: String,
+    @Json(name = "targetLatitude") val latitude: String,
+    @Json(name = "targetLongitude") val longitude: String,
+    @Json(name = "mateCount") val mateCount: Long,
+    @Json(name = "mates") val mates: Mates,
+    @Json(name = "invitedCode") val invitedCode: String,
+) {
+    data class Mates(
+        @Json(name = "mate") val mate: List<Mate>
+    )
+
+    data class Mate(
+        @Json(name = "nickname") val nickname: String,
+    )
+}
