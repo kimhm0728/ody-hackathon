38c642e6ed5573bdc3da1a1c14478d4db74d40b2
kimhyemin <kimhm0728@naver.com>
2024-07-16 02:04:15 +0900

    feat: 도착지 입력받는 기능 구현

--- android/Ody/app/src/main/java/org/hackathon/ody/presentation/MainViewModel.kt
+++ android/Ody/app/src/main/java/org/hackathon/ody/presentation/MainViewModel.kt
@@ -7,8 +7,16 @@ class MainViewModel : ViewModel() {
     private val startingPointLatitude: MutableLiveData<String> = MutableLiveData()
     private val startingPointLongitude: MutableLiveData<String> = MutableLiveData()
 
+    private val destinationLatitude: MutableLiveData<String> = MutableLiveData()
+    private val destinationLongitude: MutableLiveData<String> = MutableLiveData()
+
     fun receiveStartingPoint(startingPointLatitude: String, startingPointLongitude: String) {
         this.startingPointLatitude.value = startingPointLatitude
         this.startingPointLongitude.value = startingPointLongitude
+    }
+
+    fun receiveDestination(destinationLatitude: String, destinationLongitude: String) {
+        this.destinationLatitude.value = destinationLatitude
+        this.destinationLongitude.value = destinationLongitude
     }
 }
