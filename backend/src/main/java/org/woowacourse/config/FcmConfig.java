5fb3c787abe8c451bc593c651c4132458eb53541
mzeong <mjeong@sookmyung.ac.kr>
2024-07-16 11:19:06 +0900

    fix: 빈 생성 실패 오류 해결

--- backend/src/main/java/org/woowacourse/config/FcmConfig.java
+++ backend/src/main/java/org/woowacourse/config/FcmConfig.java
@@ -6,6 +6,7 @@ import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
 import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
@@ -13,12 +14,21 @@ import org.springframework.core.io.ClassPathResource;
 @Slf4j
 public class FcmConfig {
 
+    @Value("${fcm.enabled}")
+    private boolean fcmEnabled;
+
     @PostConstruct
     public void initialize() {
+        if (!fcmEnabled) {
+            log.info("Fcm 설정이 비활성화되었습니다.");
+            return;
+        }
+
         try {
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(
-                            GoogleCredentials.fromStream(new ClassPathResource("ody-hackathon-firebase-adminsdk-6uscb-594231ea72.json").getInputStream()))
+                            GoogleCredentials.fromStream(new ClassPathResource(
+                                    "ody-hackathon-firebase-adminsdk-6uscb-594231ea72.json").getInputStream()))
                     .build();
 
             FirebaseApp.initializeApp(options);
